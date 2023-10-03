5e8147badf36aace53e08e8ae54e311ad2a6cf96
Aditya Panday <105940283+adityapanday@users.noreply.github.com>
2023-10-03 11:24:28 +0530

    done adding name in commment

--- controllers/Comment_Controler.js
+++ controllers/Comment_Controler.js
@@ -2,20 +2,37 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 module.exports.create = async(req , res)=>{
-   try {
-    const a = Comment.create({
-        comment : req.body.comment,
-        user:req.user._id,
-        post:req.body.post
+//    try {
+//     const a = Comment.create({
+//         comment : req.body.comment,
+//         user:req.user._id,
+//         post:req.body.post
             
-    });
-    if(!a){
-        console.log("commnt not created........");
-        return res.redirect('back');
-    }
-    console.log('commet created Sucessfully');
-    return res.redirect('back');
-   } catch (error) {
+//     });
+//     if(!a){
+//         console.log("commnt not created........");
+//         return res.redirect('back');
+//     }
+//     Post.comments.push(a);
+//     Post.save();
+    
+//     console.log('commet created Sucessfully');
+//     return res.redirect('back');
+//    } 
+try{
+    let post = await Post.findById(req.body.post);
+    if (post) {
+      let comment = await Comment.create({
+        comment: req.body.comment,
+        user: req.user._id,
+        post: req.body.post
+      });
+      post.comments.push(comment);
+       post.save();
+       console.log('commet created Sucessfully' + comment);
+      res.redirect('/');
+    } 
+}catch (error) {
     console.log("commnt not created........in catch" + error);    
         return res.redirect('back');
    }
