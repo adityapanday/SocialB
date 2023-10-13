913c00ed768b90c743bbe1eb5f33bc5fd1da1052
Aditya Panday <105940283+adityapanday@users.noreply.github.com>
2023-10-13 16:58:52 +0530

    ajax done

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -1,53 +1,97 @@
-{
-let createPost = () => {
-    const newPost = $('#create-post');
-    newPost.submit(function(e) {
-      e.preventDefault();
-      // Now, how to submit
-      $.ajax({
-        type: 'post',
-        url: "/post/posts",
-        data: newPost.serialize(), 
-        success: function(data) { 
-          console.log(data);
-          const newPostElement = newDom(data.data.post);
-        // console.log('done');
+// {
+// let createPost = () => {
+//     const newPost = $('#create-post');
+//     newPost.submit(function(e) {
+//       e.preventDefault();
+//       // Now, how to submit
+//       $.ajax({
+//         type: 'post',
+//         url: "/post/posts",
+//         data: newPost.serialize(), 
+//         success: function(data) { 
+//           console.log(data);
+//           const newPostElement = newDom(data.data.post);
+//         // console.log('done');
        
-        $('#post_container').prepend(newPostElement);
-        },
-        error: function(error) {
-          console.log(error.responseText);
-        }
-      });
-    });
-  }
+//         $('#post_container>ul').prepend(newPostElement);
+//         },
+//         error: function(error) {
+//           console.log(error.responseText);
+//         }
+//       });
+//     });
+//   }
 
 
-  let newDom = function(post){
-    return $(`<li id="post_${p.id}">
+//   let newDom = function(post){
+//     return $(`<li id="post_${p.id}">
     
-        <a href="/post/destroy/${ p.id }"  class="common-class-delete"> Delete</a>
+//         <a href="/post/destroy/${ p.id }"  class="common-class-delete"> Delete</a>
     
-    <div class="content">
-    ${p.Content }
-    </div>
-    <div class="username">
-    ${ p.User.Name }
-    </div>
+//     <div class="content">
+//     ${p.Content }
+//     </div>
+//     <div class="username">
+//     ${ p.User.Name }
+//     </div>
       
-    <div>
-    <form action="/comment/create" method="post">
-        <input type="text" name="comment" placeholder="Comments">
-        <input type="hidden" name="post" value="${p._id }">
-        <input type="submit" value="Comment">
-    </form>
-</div>
+//     <div>
+//     <form action="/comment/create" method="post">
+//         <input type="text" name="comment" placeholder="Comments">
+//         <input type="hidden" name="post" value="${p._id }">
+//         <input type="submit" value="Comment">
+//     </form>
+// </div>
 
 
 
 
-</li>`)
-  }
+// </li>`)
+//   }
   
-  createPost();
-}
+//   createPost();
+// }
+
+// $(document).ready(function () {
+//   let createPost = () => {
+//     const newPost = $('#create-post');
+//     newPost.submit(function (e) {
+//       e.preventDefault();
+//       // Now, how to submit
+//       $.ajax({
+//         type: 'post',
+//         url: "/post/posts",
+//         data: newPost.serialize(),
+//         success: function (data) {
+//           console.log(data);
+//           const newPostElement = newDom(data.data.post);
+//           $('#post_container>ul').prepend(newPostElement);
+//         },
+//         error: function (error) {
+//           console.log(error.responseText);
+//         }
+//       });
+//     });
+//   }
+
+//   let newDom = function (p) {
+//     return $(`<li id="post_${p.id}">
+//         <a href="/post/destroy/${p.id}" class="common-class-delete"> Delete</a>
+//         <div class="content">
+//           ${p.Content}
+//         </div>
+//         <div class="username">
+//           ${p.User.Name}
+//         </div>
+//         <div>
+//           <form action="/comment/create" method="post">
+//             <input type="text" name="comment" placeholder="Comments">
+//             <input type="hidden" name="post" value="${p._id}">
+//             <input type="submit" value="Comment">
+//           </form>
+//         </div>
+//       </li>`);
+//   }
+
+//   createPost();
+// });
